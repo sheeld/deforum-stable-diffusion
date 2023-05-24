c7711522f8664963b3f5f2cee7485169b95b48d3
sheeldrum17 <sheelvdoshi@gmail.com>
2023-05-24 07:05:05 -0500

    Update model_load.py

--- helpers/model_load.py
+++ helpers/model_load.py
@@ -173,7 +173,7 @@ def load_model(root, load_on_run_all=True, check_sha256=True, map_location="cuda
             'url': 'https://huggingface.co/hakurei/waifu-diffusion-v1-3/resolve/main/wd-v1-3-float16.ckpt',
             'requires_login': False,
             },
-        "sadoshi-ai-v0-3.ckpt": {
+        "sadoshi-ai-v0-3.safetensors": {
             'sha256': '4199BCDD147E11328D5F3560301D5A7AB4AC7EEEFBF49DC3EB663CB3E772B9AC',
             'url': 'https://civitai.com/api/download/models/46846',
             'requires_login': False,
