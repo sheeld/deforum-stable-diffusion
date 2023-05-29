2e19485da2d2e6e6f5f52ad1032a87c9304e8c7a
sheeldrum17 <sheelvdoshi@gmail.com>
2023-05-29 03:04:02 -0500

    Update model_load.py

--- helpers/model_load.py
+++ helpers/model_load.py
@@ -174,7 +174,7 @@ def load_model(root, load_on_run_all=True, check_sha256=True, map_location="cuda
             'requires_login': False,
             },
         "sadoshi-ai-v0-3.safetensors": {
-            'sha256': '4199BCDD147E11328D5F3560301D5A7AB4AC7EEEFBF49DC3EB663CB3E772B9AC',
+            'sha256': '4199bcdd147e11328d5f3560301d5a7ab4ac7eeefbf49dc3eb663cb3e772b9ac',
             'url': 'https://civitai.com/api/download/models/46846',
             'requires_login': False,
             },
